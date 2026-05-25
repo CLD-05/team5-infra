2ee84de5357ee361a0e37a9bfba2ef9b087cfb10
Containerxox <containerxox@gmail.com>
2026-05-25 23:52:59 +0900

    chore: eks 모듈 수정

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -22,3 +22,8 @@ output "eks_node_group_name" {
   description = "EKS managed node group name"
   value       = aws_eks_node_group.this.node_group_name
 }
+
+output "eks_node_group_arn" {
+  description = "EKS managed node group ARN"
+  value       = aws_eks_node_group.main.arn
+}
